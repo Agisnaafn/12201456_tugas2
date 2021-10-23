ece8e4e3d60862153a7d7272eb353e88b2015fe9
Agisnaafn <agisnaa696@gmail.com>
2021-10-23 15:54:29 +0700

    menambahkan framework

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+public function index()
+{
+echo "<h1>Perkenalkan</h1>";
+echo"Nama saya Nur Afni Agisna
+Saya tinggal di daerah dusun Kendal RT03/RW02 Desa Sukahaji Kecamatan Cihaurbeuti Kabupaten Ciamis
+Hobi saya yang saya sukai adalah 
+Menyanyi";
+}
+}
